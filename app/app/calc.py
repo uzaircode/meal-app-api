d8cc6c73ef6d640f133c830f280872b52799ef15
Nik Uzair <nikuzairsc@gmail.com>
2023-07-22 13:41:04 +0800

    Install PostgreSQL database adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """ Add x and y and return result. """
+    return x + y
+
+def substract(x, y):
+    """ Substract x and y and return result. """
+    return y - x
