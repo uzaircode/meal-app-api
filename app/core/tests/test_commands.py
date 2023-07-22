a66777f3bcb7eed733980120f2358d00357e973c
Nik Uzair <nikuzairsc@gmail.com>
2023-07-22 14:41:46 +0800

    Fixed linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,8 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
     def test_wait_for_db_ready(self):
         pass
