222e3e2b151805ba6bad9686ace90052bed3c4df
Nik Uzair <nikuzairsc@gmail.com>
2023-07-25 11:53:38 +0800

    Implement recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -49,11 +50,11 @@ class Recipe(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    title = models.CharField(max_length=255),
-    description = models.TextField(blank=True),
-    time_minutes = models.IntegerField(),
-    price = models.DecimalField(max_digits=5, decimal_places=2),
-    link = models.CharField(max_length=255, blank=True),
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.title
